1a54adc4f56cf6b892445ed51972063e5d8348ff
Samina Bibi <saminabibi847@gmail.com>
2022-10-07 11:24:52 +0100

    Added class for Java methods of storage management

---
+++ src/FileOutputStreamStorage.java
@@ -0,0 +1,29 @@
+public class FileOutputStreamStorage implements IStorageSystem {
+
+    @Override
+    public void prepareStorage() {
+        // TODO Auto-generated method stub
+        
+        
+
+    }
+
+    @Override
+    public void store(byte[] data) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void closeStorage() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void reportAnalysis() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
